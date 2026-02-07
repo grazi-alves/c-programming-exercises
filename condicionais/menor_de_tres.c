20ab01e16cbf7bebbc73ff5608b4cba4773803d7
Graziele Alves <graziialves1410@gmail.com>
2026-02-07 17:03:36 -0300

    Rename menor_de_tres.c to condicionais/menor_de_tres.c

--- condicionais/menor_de_tres.c
+++ condicionais/menor_de_tres.c
@@ -3,8 +3,8 @@
 
 int main(){
 /*Problema "menor_de_tres"
-Fazer um programa para ler três números inteiros. Em seguida, mostrar qual o menor dentre os três
-números lidos. Em caso de empate, mostrar apenas uma vez.*/
+Fazer um programa para ler trÃªs nÃºmeros inteiros. Em seguida, mostrar qual o menor dentre os trÃªs
+nÃºmeros lidos. Em caso de empate, mostrar apenas uma vez.*/
 
     int a, b, c;
 
