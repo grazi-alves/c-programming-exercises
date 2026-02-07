4e76679ce44063ef15459956fc13572b84d76b3b
Graziele Alves <graziialves1410@gmail.com>
2026-02-07 16:58:00 -0300

    Rename Pares_consecutivos.c to repetitivas/Pares_consecutivos.c

--- repetitivas/Pares_consecutivos.c
+++ repetitivas/Pares_consecutivos.c
@@ -2,10 +2,10 @@
 
 int main(){
 /* Problema "pares_consecutivos" (adaptado de URI 1159)
-O programa deve ler um valor inteiro X indefinidas vezes. (O programa ir· parar quando o valor de X
+O programa deve ler um valor inteiro X indefinidas vezes. (O programa ir√° parar quando o valor de X
 for igual a 0). Para cada X lido, imprima a soma dos 5 pares consecutivos a partir de X, inclusive o X
-, se for par. Se o valor de entrada for 4, por exemplo, a saÌda deve ser 40, que È o resultado da operaÁ„o:
-4+6+8+10+12, enquanto que se o valor de entrada for 11, por exempo, a saÌda deve ser 80, que È a
+, se for par. Se o valor de entrada for 4, por exemplo, a sa√≠da deve ser 40, que √© o resultado da opera√ß√£o:
+4+6+8+10+12, enquanto que se o valor de entrada for 11, por exempo, a sa√≠da deve ser 80, que √© a
 soma de 12+14+16+18+20.*/
 
     int x, soma;
