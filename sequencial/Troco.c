6c3ac79a9f527c718b26bbfdbb660a2e30208025
Graziele Alves <graziialves1410@gmail.com>
2026-02-07 16:26:11 -0300

    Rename Troco.c to sequencial/Troco.c

--- sequencial/Troco.c
+++ sequencial/Troco.c
@@ -2,7 +2,7 @@
 
 /*Problema "troco"
 Fazer um programa para calcular o troco no processo de pagamento de um produto de uma mercearia.
-O programa deve ler o preÁo unit·rio do produto, a quantidade de unidades compradas deste produto,
+O programa deve ler o pre√ßo unit√°rio do produto, a quantidade de unidades compradas deste produto,
 e o valor em dinheiro dado pelo cliente (suponha que haja dinheiro suficiente). Seu programa deve
 mostrar o valor do troco a ser devolvido ao cliente.*/
 
