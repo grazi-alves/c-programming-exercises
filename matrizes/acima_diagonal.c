78fa8a236573be5946983d9b852a4bc16fbf37a2
Graziele Alves <graziialves1410@gmail.com>
2026-02-07 17:17:59 -0300

    Rename acima_diagonal.c to matrizes/acima_diagonal.c

--- matrizes/acima_diagonal.c
+++ matrizes/acima_diagonal.c
@@ -2,10 +2,10 @@
 
 
 /*Problema "acima_diagonal"
-Ler um inteiro N (máximo = 10) e uma matriz quadrada de ordem N
-contendo números inteiros. Mostrar a soma dos elementos acima da
-diagonal principal. Um exemplo de números acima da diagonal
-principal é mostrado ao lado (no caso as células com fundo cinza).*/
+Ler um inteiro N (mÃ¡ximo = 10) e uma matriz quadrada de ordem N
+contendo nÃºmeros inteiros. Mostrar a soma dos elementos acima da
+diagonal principal. Um exemplo de nÃºmeros acima da diagonal
+principal Ã© mostrado ao lado (no caso as cÃ©lulas com fundo cinza).*/
 
 int main(){
 
