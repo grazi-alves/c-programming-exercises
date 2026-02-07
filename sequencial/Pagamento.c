80af2c6fae0a56173346aa0491dfab576d885145
Graziele Alves <graziialves1410@gmail.com>
2026-02-07 16:04:51 -0300

    Rename Pagamento.c to sequencial/Pagamento.c

--- sequencial/Pagamento.c
+++ sequencial/Pagamento.c
@@ -3,8 +3,8 @@
 #include <string.h>
 
 int main(){
-/* "Problema pagamento " Fazer um programa para ler o nome de um(a) funcion·rio(a), o valor que ele(a) recebe por hora, e a
-quantidade de horas trabalhadas por ele(a). Ao final, mostrar o valor do pagamento do funcion·rio com
+/* "Problema pagamento " Fazer um programa para ler o nome de um(a) funcion√°rio(a), o valor que ele(a) recebe por hora, e a
+quantidade de horas trabalhadas por ele(a). Ao final, mostrar o valor do pagamento do funcion√°rio com
 uma mensagem explicativa, conforme exemplo. */
 
     char nome[50];
