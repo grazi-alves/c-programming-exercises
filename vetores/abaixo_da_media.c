3b1d5804eec36df61454ba5f7b5cf84853b2a795
Graziele Alves <graziialves1410@gmail.com>
2026-02-07 17:14:05 -0300

    Rename abaixo_da_media.c to vetores/abaixo_da_media.c

--- vetores/abaixo_da_media.c
+++ vetores/abaixo_da_media.c
@@ -2,9 +2,9 @@
 
 int main(){
     /* Problema "abaixo_da_media"
-    Fazer um programa para ler um número inteiro N e depois um vetor de N números reais. Em seguida,
-    mostrar na tela a média aritmética de todos elementos com três casas decimais. Depois mostrar todos
-    os elementos do vetor que estejam abaixo da média, com uma casa decimal cada.*/
+    Fazer um programa para ler um nÃºmero inteiro N e depois um vetor de N nÃºmeros reais. Em seguida,
+    mostrar na tela a mÃ©dia aritmÃ©tica de todos elementos com trÃªs casas decimais. Depois mostrar todos
+    os elementos do vetor que estejam abaixo da mÃ©dia, com uma casa decimal cada.*/
 
 
     int n ;
