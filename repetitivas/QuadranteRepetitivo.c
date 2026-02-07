91963e63d01ea3e9ee300e45cfd22f0a692e45c3
Graziele Alves <graziialves1410@gmail.com>
2026-02-07 16:59:58 -0300

    Rename QuadranteRepetitivo.c to repetitivas/QuadranteRepetitivo.c

--- repetitivas/QuadranteRepetitivo.c
+++ repetitivas/QuadranteRepetitivo.c
@@ -6,7 +6,7 @@ int main()
     Problema "quadrante" (adaptado de URI 1115)
     Escreva um programa para ler as coordenadas (X,Y) de uma quantidade indeterminada de pontos no
     sistema cartesiano. Para cada ponto escrever o quadrante a que ele pertence (Q1, Q2, Q3 ou Q4). O
-    algoritmo ser· encerrado quando pelo menos uma de duas coordenadas for NULA (nesta situaÁ„o sem
+    algoritmo ser√° encerrado quando pelo menos uma de duas coordenadas for NULA (nesta situa√ß√£o sem
     escrever mensagem alguma). */
 
 
