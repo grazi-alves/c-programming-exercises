a910165535af2a96456519d424ffc05879fba91c
Graziele Alves <graziialves1410@gmail.com>
2026-02-07 17:15:08 -0300

    Rename aprovados.c to vetores/aprovados.c

--- vetores/aprovados.c
+++ vetores/aprovados.c
@@ -3,8 +3,8 @@
 int main(){
 
     /*Fazer um programa para ler um conjunto de N nomes de alunos, bem como as notas que eles tiraram
-    no 1º e 2º semestres. Cada uma dessas informações deve ser armazenada em um vetor. Depois, imprimir
-    os nomes dos alunos aprovados, considerando aprovados aqueles cuja média das notas seja maior ou
+    no 1Âº e 2Âº semestres. Cada uma dessas informaÃ§Ãµes deve ser armazenada em um vetor. Depois, imprimir
+    os nomes dos alunos aprovados, considerando aprovados aqueles cuja mÃ©dia das notas seja maior ou
     igual a 6.0 (seis). */
 
     int n, i;
