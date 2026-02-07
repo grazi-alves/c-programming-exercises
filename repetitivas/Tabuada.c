310e3d076f31dc69a1af620e387762abe79114d3
Graziele Alves <graziialves1410@gmail.com>
2026-02-07 16:57:42 -0300

    Rename Tabuada.c to repetitivas/Tabuada.c

--- repetitivas/Tabuada.c
+++ repetitivas/Tabuada.c
@@ -3,7 +3,7 @@
 int main()
 {
     /* Problema "tabuada"
-    Ler um número inteiro N, daí mostrar na tela a tabuada de N para 1 a 10, conforme exemplo.*/
+    Ler um nÃºmero inteiro N, daÃ­ mostrar na tela a tabuada de N para 1 a 10, conforme exemplo.*/
 
     int x, i, mult;
 
