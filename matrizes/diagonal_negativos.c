86d42b7e4f6335d28bedf638bc2c6399b83ea6e5
Graziele Alves <graziialves1410@gmail.com>
2026-02-07 17:17:39 -0300

    Rename diagonal_negativos.c to matrizes/diagonal_negativos.c

--- matrizes/diagonal_negativos.c
+++ matrizes/diagonal_negativos.c
@@ -4,8 +4,8 @@
 int main(){
 
     /*Problema "diagonal_negativos"
-    Fazer um programa para ler um número inteiro N (máximo = 10) e uma matriz quadrada de ordem N
-    contendo números inteiros. Em seguida, mostrar a diagonal principal e a quantidade de valores
+    Fazer um programa para ler um nÃºmero inteiro N (mÃ¡ximo = 10) e uma matriz quadrada de ordem N
+    contendo nÃºmeros inteiros. Em seguida, mostrar a diagonal principal e a quantidade de valores
     negativos da matriz.*/
 
     int n, negativos;
