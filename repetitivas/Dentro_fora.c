eb10f39e857d1da8a556d6218b648f543c9f5d85
Graziele Alves <graziialves1410@gmail.com>
2026-02-07 16:55:16 -0300

    Rename Dentro_fora.c to repetitivas/Dentro_fora.c

--- repetitivas/Dentro_fora.c
+++ repetitivas/Dentro_fora.c
@@ -4,8 +4,8 @@ int main()
 {
     /*
      Problema "dentro_fora" (adaptado de URI 1072)
-     Leia um valor inteiro N. Este valor ser· a quantidade de valores inteiros X que ser„o lidos em seguida.
-     Mostre quantos destes valores X est„o dentro do intervalo [10,20] e quantos est„o fora do intervalo,
+     Leia um valor inteiro N. Este valor ser√° a quantidade de valores inteiros X que ser√£o lidos em seguida.
+     Mostre quantos destes valores X est√£o dentro do intervalo [10,20] e quantos est√£o fora do intervalo,
      conforme exemplo*/
 
 
