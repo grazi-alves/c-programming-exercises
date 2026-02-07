fd6bccdb03cd292fc58ff5fb10b0defb081f6bce
Graziele Alves <graziialves1410@gmail.com>
2026-02-07 16:30:26 -0300

    Rename Fatorial.c to repetitivas/Fatorial.c

--- repetitivas/Fatorial.c
+++ repetitivas/Fatorial.c
@@ -4,7 +4,7 @@
 int main(){
 
     /* Problema "fatorial" (adaptado de URI 1153)
-    Fazer um programa para ler um número natural N (valor máximo: 15), e depois calcular e mostrar o
+    Fazer um programa para ler um nÃºmero natural N (valor mÃ¡ximo: 15), e depois calcular e mostrar o
     fatorial de N. */
 
 
