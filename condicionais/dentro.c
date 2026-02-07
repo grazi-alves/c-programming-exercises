6e7ff92e094d1e7e58c519251058dc0ebd9fd6d1
Graziele Alves <graziialves1410@gmail.com>
2026-02-07 17:20:38 -0300

    Rename dentro.c to condicionais/dentro.c

--- condicionais/dentro.c
+++ condicionais/dentro.c
@@ -2,7 +2,7 @@
 
 int main(){
     /*Problema "sequencia_impares" (adaptado de URI 1067)
-    Leia um valor inteiro X. Em seguida mostre os ímpares de 1 até X, um valor por linha, inclusive o X,
+    Leia um valor inteiro X. Em seguida mostre os Ã­mpares de 1 atÃ© X, um valor por linha, inclusive o X,
     se for o caso.
     Professor:*/
 
